cec3339ca0e9098fcdf908f848517996650421e2
Brad Gardner <bradgardner@sevenhillstechnology.com>
2019-06-23 19:29:47 -0400

    added tracing to order service

--- order-service/index.js
+++ order-service/index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const ElasticAPM = require('elastic-apm-node');
+
+const apm = ElasticAPM.start({
+    serviceName: 'demo-order',
+  
+    secretToken: 'CjnbD2bsszn2A4kZz2',
+  
+    serverUrl: 'https://dc663cdec89747d4873018cae96fb96f.apm.us-east-1.aws.cloud.es.io:443',
+    captureBody: 'all',
+    logLevel: 'info'
+});
+
 const Hapi = require('hapi');
 const Elasticsearch = require('elasticsearch');
 const config = require('./config');
